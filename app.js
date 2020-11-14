749fcd6849f9dc66e141c2648526418ffa86d53b
Diego Gressler <diego.cardoso.gressler@gmail.com>
2020-11-14 16:52:49 -0300

    Ajuste funções e começo da manipulação do html

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Módulos do node e inicialização de váriaveis
 const express = require('express');
 const server = express();
+//server.set('view engine', 'ejs');
 const router = express.Router();
 const fs = require('fs');
 server.use(express.json({extended: true}));
@@ -26,19 +27,52 @@ const writeFile = (content) => {
 // Rota de busca
 router.get('/', function(req, res){
     const content = readFile();
-    console.log(content);
+    //console.log(content);
     res.send(content);
+    //res.render('./index',{ lista : content });
 });
 
 // Rota de inserção
 router.post('/', function(req, res){
     const currentContent = readFile();
-    const {cliente_id, nome, endereco, cep, data_nascimento, telefone} = req.body;
-    currentContent.push({cliente_id, nome, endereco, cep, data_nascimento, telefone});
+    const {nome, endereco, cep, data_nascimento, telefone} = req.body;
+    const id_cliente = Math.random().toString(32).substr(2.9);
+    currentContent.push({id_cliente, nome, endereco, cep, data_nascimento, telefone});
     writeFile(currentContent);
     res.send(currentContent);
 });
 
+// Rota de exclusão
+router.delete('/:id_cliente', function(req, res){
+    const {id_cliente} = req.params;
+    const currentContent = readFile();
+    const selectedItem = currentContent.findIndex((item) => item.id_cliente === id_cliente);
+    currentContent.splice(selectedItem, 1);
+    writeFile(currentContent);
+    res.send(true);
+});
+
+// Rota de update
+router.put('/:id_cliente', function(req, res){
+    const {id_cliente} = req.params;
+    const {nome, endereco, cep, data_nascimento, telefone} = req.body;
+    const currentContent = readFile();
+    const selectedItem = currentContent.findIndex((item) => item.id_cliente === id_cliente);
+    const {id_cliente: cId, nome: cNome, endereco: cEndereco, cep: cCep, data_nascimento: cData, telefone: cTelefone} = currentContent[selectedItem];
+
+    const newContato = {
+        id_cliente: cId,
+        nome: nome ? nome : cNome,
+        endereco: endereco ? endereco : cEndereco,
+        cep: cep ? cep : cCep,
+        data_nascimento: data_nascimento ? data_nascimento : cData,
+        telefone: telefone ? telefone : cTelefone
+    }
+
+    currentContent[selectedItem] = newContato;
+    writeFile(currentContent);
+    res.send(newContato);
+});
 
 // Inicializa o servidor
 server.use(router);

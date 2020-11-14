71cb6aceb5b9f797e0d439fb6f89ffe839fb5b71
Diego Gressler <diego.cardoso.gressler@gmail.com>
2020-11-14 13:36:12 -0300

    Ajuste de código

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
-const http = require("http");
+// Módulos do node e inicialização de váriaveis
+const express = require('express');
+const server = express();
+const router = express.Router();
+const fs = require('fs');
+server.use(express.json({extended: true}));
 
-/* Váriaveis para criação do servidor */
+// Váriaveis para inicialização do servidor
 const host = 'localhost';
 const port = 3000;
 
-const requestListener = function (req, res) {
-    /*res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(`{"message": "This is a JSON response"}`);*/
-};
+// Leitura do arquivo
+const readFile = () => {
+    const content = fs.readFileSync('./data/contatos.json', 'utf-8');
+    return(JSON.parse(content));
+}
 
-/* Inicialização do servidor */
-const server = http.createServer(requestListener);
+// Escrita do arquivo
+const writeFile = (content) => {
+    const updateFile =  JSON.stringify(content);
+    fs.writeFileSync('./data/contatos.json', updateFile, 'utf-8');
+}
+
+// Rotas de acesso
+
+// Rota de busca
+router.get('/', function(req, res){
+    const content = readFile();
+    console.log(content);
+    res.send(content);
+});
+
+// Rota de inserção
+router.post('/', function(req, res){
+    const currentContent = readFile();
+    const {cliente_id, nome, endereco, cep, data_nascimento, telefone} = req.body;
+    currentContent.push({cliente_id, nome, endereco, cep, data_nascimento, telefone});
+    writeFile(currentContent);
+    res.send(currentContent);
+});
+
+
+// Inicializa o servidor
+server.use(router);
+
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
